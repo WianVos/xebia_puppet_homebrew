7ddea6546f2f40dc6efe654405d8b85587f673c1
Wian Vos <wvos@xebia.com>
2011-11-27 22:26:48 +0000

    done for now

--- opendj/manifests/install.pp
+++ opendj/manifests/install.pp
@@ -2,14 +2,14 @@ class opendj::install(
 	$basedn="dc=example,dc=com",
 	$ldapport="389",
 	$adminport="4444",
-	$rootuser="xadmin",
+	$rootuser="Directory Manager",
 	$rootpassword="xita01"
 ){
 
 	include opendj::params
 	
 	exec {"opendj install":
-		command => "${opendj::params::installpath}/OpenDJ-2.4.4/setup --cli --baseDN ${basedn} --ldapPort ${ldapport} --adminConnectorPort ${adminport} --rootUserDN cn=${rootuser} --rootUserPassword ${rootpassword} --no-prompt --noPropertiesFile && touch ${opendj::params::installpath}/OpenDJ-2.4.4/puppetinstalledopendj.txt ",
+		command => "${opendj::params::installpath}/OpenDJ-2.4.4/setup --cli --baseDN ${basedn} --ldapPort ${ldapport} --adminConnectorPort ${adminport} --rootUserDN cn=\'${rootuser}\' --rootUserPassword ${rootpassword} --no-prompt --noPropertiesFile && touch ${opendj::params::installpath}/OpenDJ-2.4.4/puppetinstalledopendj.txt ",
 		creates => "${opendj::params::installpath}/OpenDJ-2.4.4/puppetinstalledopendj.txt",
 		}
 
