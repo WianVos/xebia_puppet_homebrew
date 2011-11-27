4cc792fca5f37b2e2151bb38dd5d2571714f44c8
Wian Vos <wvos@xebia.com>
2011-11-27 21:08:29 +0000

    added services setup for ubuntu

--- opendj/manifests/ubuntu/service.pp
+++ opendj/manifests/ubuntu/service.pp
@@ -1,9 +1,19 @@
 class opendj::ubuntu::service(
+	$running_user="root"
 	)
 {
+	include opendj::params
 
+        exec {"opendj service setup":
+                command => "${opendj::params::installpath}/OpenDJ-2.4.4/bin/create-rc-script -f /etc/init.d/opendj -u root",
+                creates => "/etc/init.d/opendj",
+		require => Exec['opendj install'],
+                }
 
-
+	service {"opendj":
+		ensure => running,
+		require => Exec['opendj service setup'],
+		}
 
 
 
